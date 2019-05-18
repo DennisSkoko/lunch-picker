d4fd2ef3b25a22f4bf19213079842b1bb7a5fb31
Dennis Skoko <dennis.skoko@hotmail.com>
2019-05-18 13:50:52 +0200

    Configures the GraphQL depending if it running in prod

--- packages/api/src/graphql.ts
+++ packages/api/src/graphql.ts
@@ -1,6 +1,12 @@
 import { ApolloServer } from 'apollo-server-lambda'
 import schema from './schema'
 
-const graphql = new ApolloServer({ schema });
+const isProd = process.env.NODE_ENV === 'production'
 
-export default graphql;
+const graphql = new ApolloServer({
+  schema,
+  tracing: !isProd,
+  playground: !isProd
+})
+
+export default graphql
