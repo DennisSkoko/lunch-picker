460c1856ebcfe6a38ba9e9e7a52d52d2c09061bb
Dennis Skoko <dennis.skoko@hotmail.com>
2019-05-23 16:34:47 +0200

    Fixes all lint issues

--- packages/app-web/src/components/App/App.js
+++ packages/app-web/src/components/App/App.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React from 'react'
 
-function App() {
+function App () {
   return (
     <h1>Lunch Picker</h1>
-  );
+  )
 }
 
-export default App;
+export default App
