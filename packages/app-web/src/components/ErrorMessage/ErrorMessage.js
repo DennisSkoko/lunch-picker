45fd129f2837765d1a98ef35324a4ea732b1532f
Dennis Skoko <dennis.skoko@hotmail.com>
2019-05-25 21:25:15 +0200

    Adds a component for showing errors

---
+++ packages/app-web/src/components/ErrorMessage/ErrorMessage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ContentFullscreen from '../ContentFullscreen'
+import Text from '../Text'
+
+function ErrorMessage () {
+  return (
+    <ContentFullscreen>
+      <Text>An error occurred :(</Text>
+    </ContentFullscreen>
+  )
+}
+
+export default ErrorMessage
