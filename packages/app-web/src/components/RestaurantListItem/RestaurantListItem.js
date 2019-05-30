5d0688024d1ce75b067a9016239679d9d79abc45
Dennis Skoko <dennis.skoko@hotmail.com>
2019-05-30 15:16:04 +0200

    Fixes lint issues

--- packages/app-web/src/components/RestaurantListItem/RestaurantListItem.js
+++ packages/app-web/src/components/RestaurantListItem/RestaurantListItem.js
@@ -1,11 +1,9 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import classNames from 'classnames'
 import Text from '../Text'
-import styles from './RestaurantListItem.module.scss'
 
-function RestaurantListItem ({ restaurant, className, ...props }) {
-  return <Text>{restaurant.name}</Text>
+function RestaurantListItem ({ restaurant, ...props }) {
+  return <Text {...props}>{restaurant.name}</Text>
 }
 
 RestaurantListItem.propTypes = {
